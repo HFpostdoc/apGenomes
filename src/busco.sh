2473dc0fe236359f975e862d86a5899701934aa1
mklau <matthewklau@fas.harvard.edu>
2018-11-16 12:53:29 -0500

    debugs busco.sh

--- src/busco.sh
+++ src/busco.sh
@@ -1,12 +1,12 @@
 ### Run this from the assemblies directory
 
 mkdir ~/BUSCOVM/busco3/output
+mkdir ~/BUSCOVM/busco3/output/summaries
 
-for i in $(ls);
+for i in $(ls apg);
 do
-    python ~/BUSCOVM/busco3/scripts/run_BUSCO.py -i $i -o ~/BUSCOVM/busco3/output/busco_$i -l ~/BUSCOVM/busco3/lineages/metazoa -m genome
-    mkdir ~/BUSCOVM/busco3/output/summaries
-    cp ~/BUSCOVM/busco3/output/busco_$i/short_summary.txt ~/BUSCOVM/busco3/output/summaries/short_summary_$i.txt
+    python ~/BUSCOVM/busco3/scripts/run_BUSCO.py -i apg/$i/$i.fna -o output_$i -l ~/BUSCOVM/busco3/lineages/eukaryota_odb9 -m genome
+    cp output_$i/short_summary.txt ~/BUSCOVM/busco3/output/summaries/short_summary_$i.txt
 done 
 
 python ~/BUSCOVM/busco3/scripts/generate_plot.py -wd python ~/BUSCOVM/busco3/output/summaries
